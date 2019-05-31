ee19ffd1a86efca4c1117db3b1a59ff070f8a4bf
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-05-31 14:59:37 -0400

    Install knex and pg

--- src/config.js
+++ src/config.js
@@ -14,22 +14,20 @@ const PORT = process.env.PORT;
 const LOG_FORMAT = process.env.LOG_FORMAT;
 
 const DB_CLIENT = 'pg';
-const DB_CONNECTION_STRING = `
-  postgres://${process.env.DB_USERNAME}
-  :${process.env.DB_PASSWORD}
-  @${process.env.DB_HOST}
-  :${process.env.DB_PORT}
-  /${process.env.DB_DATABASE}
-`;
+const DB_CONNECTION_STRING = `postgres://`
+  + `${process.env.DB_USERNAME}:`
+  + `${process.env.DB_PASSWORD}@`
+  + `${process.env.DB_HOST}:`
+  + `${process.env.DB_PORT}/`
+  + `${process.env.DB_DATABASE}`;
 
 const TEST_DB_CLIENT = 'pg';
-const TEST_DB_CONNECTION_STRING = `
-  postgres://${process.env.TEST_DB_USERNAME}
-  :${process.env.TEST_DB_PASSWORD}
-  @${process.env.TEST_DB_HOST}
-  :${process.env.TEST_DB_PORT}
-  /${process.env.TEST_DB_DATABASE}
-`;
+const TEST_DB_CONNECTION_STRING = `postgres://`
+  + `${process.env.TEST_DB_USERNAME}:`
+  + `${process.env.TEST_DB_PASSWORD}@`
+  + `${process.env.TEST_DB_HOST}:`
+  + `${process.env.TEST_DB_PORT}/`
+  + `${process.env.TEST_DB_DATABASE}`;
 
 module.exports = {
   NODE_ENV,
