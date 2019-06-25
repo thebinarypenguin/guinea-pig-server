ead97d978e81a5baf58fb6e002c0ee2193f1d4e8
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-06-25 11:22:45 -0400

    Handle Heroku's DATABASE_URL config

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ const { PORT } = process.env;
 const { LOG_FORMAT } = process.env;
 
 const DB_CLIENT = 'pg';
-const DB_CONNECTION_STRING = 'postgres://'
+let DB_CONNECTION_STRING = 'postgres://'
   + `${process.env.DB_USERNAME}:`
   + `${process.env.DB_PASSWORD}@`
   + `${process.env.DB_HOST}:`
@@ -18,12 +18,20 @@ const DB_CONNECTION_STRING = 'postgres://'
   + `${process.env.DB_DATABASE}`;
 
 const TEST_DB_CLIENT = 'pg';
-const TEST_DB_CONNECTION_STRING = 'postgres://'
+let TEST_DB_CONNECTION_STRING = 'postgres://'
   + `${process.env.TEST_DB_USERNAME}:`
   + `${process.env.TEST_DB_PASSWORD}@`
   + `${process.env.TEST_DB_HOST}:`
   + `${process.env.TEST_DB_PORT}/`
   + `${process.env.TEST_DB_DATABASE}`;
+
+/*
+ * If DATABASE_URL is present we are probably using Heroku PostgreSQL
+ */
+if (process.env.DATABASE_URL) {
+  DB_CONNECTION_STRING      = process.env.DATABASE_URL;
+  TEST_DB_CONNECTION_STRING = null;
+}
 
 module.exports = {
   NODE_ENV,
